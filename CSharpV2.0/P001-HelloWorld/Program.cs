0da5d69899a176a179660dff17002205b778244d
Summy Singh <summysingh86@gmail.com>
2021-10-22 19:06:08 +0530

    Hello World Added

---
+++ CSharpV2.0/P001-HelloWorld/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace P001_HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
