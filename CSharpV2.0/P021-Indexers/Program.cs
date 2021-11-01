973c0af47c76e5b83f457dfc37b8f3ae56d5b504
Summy Singh <summysingh86@gmail.com>
2021-11-01 15:27:51 +0530

    Indexer program added

---
+++ CSharpV2.0/P021-Indexers/Program.cs
@@ -0,0 +1,23 @@
+﻿using P021_IndexersLibrary;
+using System;
+
+namespace P021_Indexers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Car c = new Car();
+
+            //call get accessor
+            Console.WriteLine(c[0]);
+            Console.WriteLine(c["first"]);
+
+            //call set accessor
+            c[0] = "TESLA";
+            Console.WriteLine("After modifier 0 index: {0}", c[0]);
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpV2.0/P021-IndexersLibrary/Class1.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P021_IndexersLibrary
+{
+    public class Car
+    {
+
+        //private field
+        private string[] _brands = new string[] { "BMW", "SKODA", "HONDA" };
+        private string[] _names = new string[] { "first", "second", "third" };
+
+        //public indexer
+        public string this [int index]
+        {
+            set
+            {
+                this._brands[index] = value;
+            }
+            get
+            {
+                return _brands[index];
+            }
+        }
+
+        //indexer overloading
+        public string this[string name]
+        {
+            set
+            {
+                this._brands[Array.IndexOf(_names,name)] = value;
+            }
+            get
+            {
+                return _brands[Array.IndexOf(_names, name)];
+            }
+        }
+
+    }
+}
