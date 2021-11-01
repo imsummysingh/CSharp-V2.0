ec75d12250261ef889b1c61127b526348b676ca3
Summy Singh <summysingh86@gmail.com>
2021-11-01 19:40:08 +0530

    method hiding & overriding prog added

---
+++ CSharpV2.0/P022-MethodHiding/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace P022_MethodHiding
+{
+    class Parent
+    {
+        public void Print()
+        {
+            Console.WriteLine("Calling from parent");
+        }
+    }
+    class Child : Parent
+    {
+        public new void Print()
+        {
+            //base.Print();
+            Console.WriteLine("Calling from child by hiding parent class method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Parent();
+            p.Print();  //calling from parent
+
+            Child c = new Child();
+            c.Print();  //calling from child
+
+            Parent p1 = new Child();
+            p1.Print();     //calling from parent
+
+            //Child c1 = new Parent();
+            //c1.Print();     //cannot implicityly convert- explicit conversion needed
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpV2.0/P023-MethodOverriding/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace P023_MethodOverriding
+{
+    class Parent
+    {
+        public virtual void Print()
+        {
+            Console.WriteLine("Calling from parent");
+        }
+    }
+
+    class Child : Parent
+    {
+        public override void Print()
+        {
+            base.Print();
+            Console.WriteLine("Calling from child by Overriding parent class method");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Parent p = new Parent();
+            p.Print();  //calling from parent
+
+            Child c = new Child();
+            c.Print();  //calling from child
+
+            Parent p1 = new Child();
+            p1.Print();     //calling from child            
+
+            Console.ReadLine();
+        }
+    }
+}
