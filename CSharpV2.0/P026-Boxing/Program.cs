bfa76972c276f8c957b2ca6b7baa188206ea6cab
Summy Singh <summysingh86@gmail.com>
2021-11-02 21:13:05 +0530

    boxing & unboxing prog added

---
+++ CSharpV2.0/P026-Boxing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P026_Boxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //primitive value
+            int x = 10;
+
+            //non-primitive value
+            //boxing
+            object obj = x;
+
+            Console.WriteLine("Before Boxing: {0}", x);
+            Console.WriteLine("After Boxing: {0}", obj);
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharpV2.0/P027-UnBoxing/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace P027_UnBoxing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //non-primitive value
+            object obj = 10;
+
+            //primitive value
+            //unboxing
+            int x = (int)obj;
+
+            Console.WriteLine("Before UnBoxing: {0}", obj);
+            Console.WriteLine("After UnBoxing: {0}", x);
+
+            Console.ReadLine();
+        }
+    }
+}
